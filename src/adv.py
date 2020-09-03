5a78316400eae0727c5a64e03c2cc8c040a93702
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-09-02 21:01:34 -0700

    The first part of the project is finished

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -21,6 +22,21 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+def parse_move(player,move):
+    room = player.current_room
+    try:
+        if move == 'n': 
+            return room.n_to
+        elif move == 's':
+            return room.s_to
+        elif move == 'e':
+            return room.e_to
+        elif move == 'w':
+            return room.w_to
+    except:
+        print("There is no room. Try it again!")
+        return room
+  
 
 # Link rooms together
 
@@ -36,8 +52,13 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+def get_user_choice(w_string):
+    choice = input(w_string)
+    return choice
 # Make a new player object that is currently in the 'outside' room.
+user = input("Enter your name: ")
+player = Player(user, room['outside'])
+print(player)
 
 # Write a loop that:
 #
@@ -49,3 +70,43 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+welcome_message = "Please enter: \nNorth n\nSouth s\nEast  e\nWest  w\nQuit  q\n"
+user_input = get_user_choice(welcome_message).lower()
+
+while True:
+    if user_input == 'q':
+        print("The game is over")
+        break
+    else:
+        if parse_move(player, user_input) is not None:
+            print(parse_move(player, user_input))
+            player.current_room = parse_move(player, user_input)
+        else:
+           print("There is no room. Try it again!") 
+
+    user_input = get_user_choice(welcome_message).lower()
+
+
+# while True:
+#     if user_input == 'q':
+#         print("The game is over")
+#         break
+#     elif user_input in {'n','s','e','w'}:
+#         if hasattr(player.current_room, f"{user_input}_to"):
+#             player.current_room = getattr(player.current_room, f"{user_input}_to")
+#         else:
+#             print("somthing")
+#     else:
+#         print(parse_move(player, user_input))
+#     #     player.current_room = parse_move(player, user_input)
+    
+#     user_input = get_user_choice(welcome_message).lower()
+
+
+
+
+
+
+         
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,23 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        
+
+    def __str__(self):
+        output = f"Room name: {self.name}, \nDescription: {self.description}\n"
+        if self.n_to:
+            output += f"north to --> {self.n_to.name}\n"
+        if self.s_to:
+            output += f"south to --> {self.s_to.name}\n"
+        if self.e_to:
+            output += f"east to  --> {self.e_to.name}\n"
+        if self.w_to:
+            output += f"west to  --> {self.w_to.name}\n"
+        return output
